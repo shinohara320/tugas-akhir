bd7fbf817e1943751b9ee87605689db112f09acc
shinohara320 <daudmanuwu@gmail.com>
2023-05-13 21:04:54 +0900

    commit dulu ga sih

--- src/homepage.php
+++ src/homepage.php
@@ -25,6 +25,12 @@
     </nav>
     <!-- navbar end -->
     <!-- slider -->
+    <h2 style="text-align:center; margin:50px 0;font-weight: 700;font-size:36px">All You NEED!</h2>
+    <p style="text-align:center;padding: 0 60px;">Discover the perfect fit for every occasion with our wide range of shoes! 
+    We've got everything you need to complete your shoe collection. 
+    From trendy sneakers to classy loafers, we've got it all. Shop now and
+    experience the ultimate convenience of having all your shoe needs 
+    in one place!</p>
     <div class="slider">
         <div class="slider-container">
             <div>
@@ -49,25 +55,49 @@
         <div class="prev" id="prev">&#10094;</div>
         <div class="next" id="next">&#10095;</div>
     </div>
-    <h2>NEW ARIVALL</h2>
-    <p style="width: 900px;margin: 10px auto;text-align: center;">Lorem ipsum dolor sit amet consectetur adipisicing 
-    elit. Ducimus dignissimos incidunt molestias minus 
-    perferendis illo ipsum voluptates minima harum veritatis 
-    iste, laudantium dicta quos. Placeat vel accusamus maxime 
-    fuga error.</p>
     <!-- slider end -->
     <!-- best-sell -->
-    <div class="ads-container">
+    <h2 style="text-align:left; margin:40px 0 40px 50px;font-weight: 700;font-size:36px">HOT Sale!</h2>
+    <p style="text-align:left; padding: 0 600px 0 50px;">
+    Get ready to turn heads with our hot selling collection - 
+    the perfect blend of style and comfort! Don't miss out on owning 
+    the most sought-after shoes in the market. Shop now and be the 
+    envy of all your friends!
+    </p>
+    <div class="ads-container" >
         <div class="left-side">
             <img src="show.php?id=19" alt="">
+            <div class="right-txt" style="position: absolute; top: 1300px;">
+                <h3>Air Jordan Retro 6</h3>
+                <a href="detail-product.php?id=19">Order</a>
+            </div>
         </div>
         <div class="right-side">
-            <img src="show.php?id=21" alt="">
-            <img src="show.php?id=34" alt="">
+            <div class="right-1" >
+                <img src="show.php?id=21" alt="">
+                <div id="txt-1" style="position: absolute; top: 1100px;">
+                    <h3>Nike Air Max Penny</h3>
+                    <a href="detail-product.php?id=21">Order</a>
+                </div>
+            </div>
+            <div class="right-2">
+                <img src="show.php?id=34" alt="">
+                <div id="txt-2" style="position: absolute; top: 1480px; right: 250px;">
+                    <h3>Nike Dunk Low</h3>
+                    <a href="detail-product.php?id=34">Order</a>
+                </div>
+            </div>
         </div>
     </div>
     <!-- best-sell end -->
     <!-- section sale -->
+    <h2 id="txt-category">For EVERYONE!</h2>
+    <p style="text-align:center; padding: 0 60px;">
+    Find the perfect shoes for everyone with our wide range of 
+    sizes, styles, and colors! Our collection offers trendy and comfortable 
+    shoes for kids, men, and women, ensuring that everyone gets the perfect 
+    fit!
+    </p>
     <div class="section-container">
         <div class="male-section">
             <img src="../public/images/nike-air.jpg" alt="">
@@ -93,6 +123,10 @@
     </div>
     <!-- section sale end -->
     <!-- testmoni -->
+    <h2 style="text-align:center; margin:40px 0;font-weight: 700;font-size:36px">What they said?</h2>
+    <p style="text-align:center;padding: 0 60px;">
+    Customer satisfaction is our top priority! We pride ourselves on providing exceptional service that keeps our customers coming back time and time again
+    </p>
     <div class="container">
         <div class="card-carousel">
           <div class="card" id="1">
@@ -123,7 +157,7 @@
     <!-- footer -->
     <div class="footer-container">
         <div class="footer-address">
-            <h3>Alamat</h2>
+            <h3>Alamat</h3>
             <p>
                 GR8M+6G5, Jalan Aneka Tambang, Trikora, Palam, 
                 Kecamatan Cempaka, Kota Banjar Baru, Kalimantan Selatan 
